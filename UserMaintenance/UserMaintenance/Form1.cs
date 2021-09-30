d9b00c6aaca14ad639cbca8e2be2cb7e439163f2
Róbert Obertol <robert.obertol@stud.uni-corvinus.hu>
2021-10-01 01:52:03 +0200

    Fileba

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ namespace UserMaintenance
         BindingList<User> users = new BindingList<User>();
         public Form1()
         {
+            InitializeComponent();
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
             listBox1.DisplayMember = "FullName";
@@ -24,7 +26,6 @@ namespace UserMaintenance
             label1.Text = Resource1.LastName;
             label2.Text = Resource1.FirstName;
             button1.Text = Resource1.Add;
-            InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -35,6 +36,22 @@ namespace UserMaintenance
                 FirstName = textBox2.Text
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                foreach (var u in users)
+                {
+                    sw.Write(u.ID);
+                    sw.Write(";");
+                    sw.Write(u.FullName);
+                    sw.WriteLine();
+                }
+            }
         }
     }
 }
